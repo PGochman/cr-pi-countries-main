3e3b254a45468569b432d03252b6940e8da91b8e
PGochman <pedrogoch55@gmail.com>
2023-11-06 15:04:35 -0300

    [FINISH] styles

--- client/src/pages/detail/Detail.jsx
+++ client/src/pages/detail/Detail.jsx
@@ -2,6 +2,15 @@ import { useParams } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { cleanDetail, getDetail } from "../../redux/actions/actions"
 import { useEffect, useState } from "react"
+import style from "./detail.module.css"
+
+const difficulties = {
+    1: "Very easy",
+    2: "Easy",
+    3: "Medium",
+    4: "Hard",
+    5: "Very hard"
+}
 
 const Detail = () => {
     const {countryId} = useParams()
@@ -32,41 +41,42 @@ const Detail = () => {
         <div>
             {countryDetail ? (
                 Object.keys(countryDetail).length !== 0 ? (
-                    <div>
-                    <h1>Name: {countryDetail.name}({countryDetail.id})</h1>
-                    <img src={countryDetail.flag} alt={countryDetail.name} />
-                    <h3>Continent: {countryDetail.continent}</h3>
-                    {countryDetail.capital ? (
-                        <h3>Capital: {countryDetail.capital}</h3>
-                    ) : (
-                        <></>
-                    )}
-                    {countryDetail.subregion ? (
-                        <h3>Sub region: {countryDetail.subregion}</h3>
-                    ) : (
-                        <></>
-                    )}
-                    <h3>Area: {countryDetail?.area} km2</h3>
-                    <h3>Population: {countryDetail.population} people</h3>
-                    {showActivities ? (
-                        <div>
-                        <h2>Activities to do: </h2>
-                        {countryDetail?.activities?.map((activity) => {
-                            return (
-                                <div key={activity.id}>
-                                    <h3>Name: {activity.name}</h3>
-                                    <h3>Season: {activity.season}</h3>
-                                    <h3>Aproximate duration: {activity.duration} hours</h3>
-                                    <h3>Difficulty (on a scale from 1 to 5): {activity.difficulty}</h3>
-                                    <h3></h3>
-                                </div>
-                            ) 
-                        })}
-                        <button onClick={handleActivities}>Hide activity info</button>
+                    <div  className={style.div}>
+                        <div className={style.detail}>
+                            <h1>{countryDetail.name}({countryDetail.id})</h1>
+                            <img className={style.flag} src={countryDetail.flag} alt={countryDetail.name} />
+                            <h3>Continent: {countryDetail.continent}</h3>
+                            {countryDetail.subregion && 
+                                <h3>Sub region: {countryDetail.subregion}</h3>
+                            }
+                            {countryDetail.capital && 
+                                <h3>Capital: {countryDetail.capital}</h3>
+                            }
+                            <h3>Area: {countryDetail?.area} km2</h3>
+                            <h3>Population: {countryDetail.population} people</h3>
                         </div>
-                    ) : (
-                        <button onClick={handleActivities}>Show activity info</button>
-                    )}
+                        {countryDetail.activities.length ? (
+                            <div className={style.activities}>
+                            <h2>Activities to do: </h2>
+                            {countryDetail?.activities?.map((activity) => {
+                                return (
+                                    <div className={style.activity} key={activity.id}>
+                                        <h2>{activity.name}</h2>
+                                        <h3>Season: {activity.season}</h3>
+                                        {activity.duration &&
+                                            <h3>Aproximate duration: {activity.duration} hours</h3>
+                                        }
+                                        <h3>Difficulty: {difficulties[activity.difficulty]}</h3>
+                                    </div>
+                                ) 
+                            })}
+                            </div>
+                        ) : (
+                            <div className={style.noActivities}>
+                                <h1>The country has no activities associated</h1>
+                                <h2>Press the create activity button above to create one!</h2>
+                            </div>
+                        )}
                     </div>
                 ) : (
                     <div>
